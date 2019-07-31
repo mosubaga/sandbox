6aa055dbb96dd5586540fa9e902dd7f3799ef0a0
mosubaga <mosubaga@icloud.com>
2019-07-30 22:15:48 -0400

    added code

--- golang/src/getapi/getapi.go
+++ golang/src/getapi/getapi.go
@@ -4,10 +4,15 @@ import (
 "fmt"
 "net/http"
 "io/ioutil"
-// "encoding/json"
+"encoding/json"
+"strconv"
 "os"
 )
 
+type Book struct {
+  ID int `json:"id"`
+  Title string `json:"title"`
+}
 
 // GetRequest http request
 func GetRequest(sURL string) []byte {
@@ -15,7 +20,7 @@ func GetRequest(sURL string) []byte {
 	oClient := &http.Client{}
 
 	req,  err := http.NewRequest(sMethod,sURL,nil)
-    resp, err := oClient.Do(req)
+  resp, err := oClient.Do(req)
 
 	if err != nil {
 		fmt.Printf("%s", err)
@@ -33,7 +38,14 @@ func GetRequest(sURL string) []byte {
 func main() {
 
   // Taken from JSONPlaceholder : https://jsonplaceholder.typicode.com/
-	var sURL = "https://jsonplaceholder.typicode.com/todos/1"
-	var byResponse = GetRequest(sURL)
-	fmt.Println(string(byResponse))
+	var sURL = "https://jsonplaceholder.typicode.com/todos"
+	var sResponse = string(GetRequest(sURL))
+
+  var stext []Book
+  json.Unmarshal([]byte(sResponse), &stext)
+
+  for i := 0; i < len(stext); i++ {
+    var sID = strconv.Itoa(stext[i].ID)
+    fmt.Println(sID + ": " + stext[i].Title)
+	}
 }
