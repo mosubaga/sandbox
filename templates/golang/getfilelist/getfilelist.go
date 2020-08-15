eaff545cdb73f9fb19a0582d7285b12ec1c14107
mosubaga <mosubaga@icloud.com>
2020-08-15 16:37:40 -0400

    Add golang files

---
+++ templates/golang/getfilelist/getfilelist.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+)
+
+func main() {
+	filePath := "/Users/nobu/Projects/scripts/sandbox/scripts/work"
+	fileList := GetFileList(filePath)
+
+	for i, file := range fileList {
+		fmt.Printf("File #%d: %s\n", i, file)
+	}
+}
+
+// GetFileList - get file list of folder
+func GetFileList(filePath string) []string {
+
+	var fileList []string
+	re, _ := regexp.Compile("^.*\\.(py)$")
+
+	err := filepath.Walk(filePath, func(path string, f os.FileInfo, err error) error {
+		if re.MatchString(path) {
+			fileList = append(fileList, path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	return fileList
+}
