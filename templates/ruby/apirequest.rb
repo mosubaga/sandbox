97c51035e3d070ff207d6fe5b7937b386a520c0a
NOBUHIKO HAYASHI <nobuhikohayashi@Komasan.local>
2020-09-05 14:15:29 -0400

    Ruby Request

---
+++ templates/ruby/apirequest.rb
@@ -0,0 +1,30 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+uri = URI('https://api.covidtracking.com/v1/states/current.json')
+response = Net::HTTP.get_response(uri)
+
+parsed = JSON.parse(response.body) # returns a hash
+
+cases  = Hash.new()
+deaths = Hash.new()
+
+parsed.each do |data|
+  sState    = data['state']
+  iPositive = data['positive']
+  iDeaths   = data['death']
+  cases[sState] = iPositive
+  deaths[sState] = iDeaths
+end
+
+cases = cases.sort_by {|k, v| -v}
+
+i = 1
+cases.each do |key, value|
+  puts "#{i},#{key},#{value},#{deaths[key]}\n"
+  i+=1
+end
+
+
+
