44c36166d903400e41534a9bcc7b547486465c88
NOBUHIKO HAYASHI <mosubaga@icloud.com>
2021-03-23 19:48:01 -0400

    Add spaces

--- templates/python_scripts/reqjson.py
+++ templates/python_scripts/reqjson.py
@@ -1,6 +1,7 @@
 import sys, shutil, os, re, codecs, pprint
 import requests, json
 
+
 def GetJSON(sURL):
     jText = requests.get(sURL).text
     parseJSON = json.loads(jText)
@@ -11,7 +12,8 @@ def GetJSON(sURL):
     for x in range(1,ListCount-3):
         print(sSourceTextList[x]['[key2]'])
 
-# ----------------------------------------------
+
+        # ----------------------------------------------
 def main():
 # ----------------------------------------------
     sURL ='[URL]'
