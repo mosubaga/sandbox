db232a0768d75943e7c281101a812d9cfe848251
Nobuhiko Hayashi <mosubaga@icloud.com>
2021-06-11 19:36:28 -0400

    Add kotlin

--- templates/kotlin/getfilelist.kts
+++ templates/kotlin/getfilelist.kts
@@ -1,18 +1,35 @@
 import java.io.File
 
-
-fun GetFileList(){
-	val sPathName="[path]"
+fun GetFileList() : MutableList<String>{
+	val sPathName="[PATH]";
+	val aFiles: MutableList<String> = mutableListOf()
 	File(sPathName).walk().forEach {
-		val sFile = it.toString()
+		val sFile = it.toString();
 		if ((sFile.contains(".py",ignoreCase=true)) || (sFile.contains(".pl",ignoreCase=true))){
-			print(sFile + "\n")
+			aFiles.add(sFile);
 		}
+	}
+	return aFiles;
+}
+
+fun GetString(aFiles: MutableList<String>) {
+
+	for (sFile in aFiles){
+    	val lines: List<String> = File(sFile).readLines()
+		val regex = "import".toRegex()
+
+    	for(line in lines){
+        	if (regex.containsMatchIn(line)){
+        		print("[$sFile] : $line\n")
+        	}
+    	}
 	}
 }
 
 fun main() {
-	GetFileList()
+	var aFiles = GetFileList();
+	GetString(aFiles)
 }
 
-main()
+main();
+
