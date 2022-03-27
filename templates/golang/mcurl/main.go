5839fa56ff494d41e17158adc6ca3f2b03daa676
Nobuhiko Hayashi <mosubaga@icloud.com>
2022-03-27 10:53:11 -0400

    Modify mcurl

--- templates/golang/mcurl/main.go
+++ templates/golang/mcurl/main.go
@@ -3,20 +3,34 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 )
 
-func PrettyString(str string) string {
+var sURL string
+var sOutFile string
+
+func init() {
+	flag.StringVar(&sURL, "url", "", "URL to curl")
+	flag.StringVar(&sOutFile, "output", "output.json", "Output file")
+	flag.Parse()
+	if sURL == "" || sOutFile == "" {
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+}
+
+func PrettyString(str *string) string {
 	var prettyJSON bytes.Buffer
 
 	// Return the input string if input has errors
-	if err := json.Indent(&prettyJSON, []byte(str), "", "    "); err != nil {
+	if err := json.Indent(&prettyJSON, []byte(*str), "", "    "); err != nil {
 		fmt.Println("WARNING: Not in JSON form despite header says it. Please see error below. Returning the raw content anyways... ")
 		fmt.Println(err)
-		return str
+		return *str
 	}
 	return prettyJSON.String()
 }
@@ -24,7 +38,7 @@ func PrettyString(str string) string {
 func mCurlGet(sURL string, sfilePath string) {
 
 	req, err := http.NewRequest("GET", sURL, nil)
-	req.Header.Add("Authorization", "[AuthHeader]")
+	req.Header.Add("Authorization", "[AuthUser]")
 	response, err := http.DefaultClient.Do(req)
 
 	if err != nil {
@@ -33,7 +47,7 @@ func mCurlGet(sURL string, sfilePath string) {
 	} else {
 		defer response.Body.Close()
 		responsetype := response.Header.Get("Content-Type")
-		fmt.Printf("Status Code: %d\n",response.StatusCode)
+		fmt.Printf("Status Code: %d\n", response.StatusCode)
 		contents, err := ioutil.ReadAll(response.Body)
 		if err != nil {
 			fmt.Printf("%s", err)
@@ -48,7 +62,7 @@ func mCurlGet(sURL string, sfilePath string) {
 			sOutput := string(contents)
 
 			if responsetype == "application/json" {
-				sOutput = PrettyString(sOutput)
+				sOutput = PrettyString(&sOutput)
 			}
 
 			f.WriteString(sOutput)
@@ -57,14 +71,5 @@ func mCurlGet(sURL string, sfilePath string) {
 }
 
 func main() {
-
-	if len(os.Args) < 3 {
-		fmt.Println("Error: Not enough number of arguments!")
-		os.Exit(1)
-	}
-
-	sURL := os.Args[1]
-	sfilePath := os.Args[2]
-	mCurlGet(sURL, sfilePath)
+	mCurlGet(sURL, sOutFile)
 }
-
