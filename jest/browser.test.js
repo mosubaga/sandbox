4d0bbd2e6fce5b0946d7bd65bd895ca519a52331
mosubaga <42124945+mosubaga@users.noreply.github.com>
2024-03-10 13:04:29 -0400

    Remove unwanted stuffs

--- jest/browser.test.js
+++
@@ -1,8 +0,0 @@
-const doescontentmatch = require('./browser');
-
-jest.setTimeout(30000);
-
-test('Sample Text', async () => {
-    const sAnswer = await doescontentmatch();
-    await expect(sAnswer).toBeFalsy();
-});
