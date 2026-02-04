4aabdc3cf2aa3c4e960488487309e9efe06a244b
Nobuhiko Hayashi <mosubaga@icloud.com>
2026-02-04 11:22:22 -0500

    installation script

---
+++ installsoftware.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+# Install required software packages using dnf package manager
+
+sudo dnf install -y  clang
+sudo dnf install -y  clang-libs
+sudo dnf install -y  boost
+sudo dnf install -y  boost-static
+sudo dnf install -y  git
+sudo dnf install -y  svn
+sudo dnf install -y  libcxx*
+sudo dnf install -y  cmake
+sudo dnf install -y  openssl
+sudo dnf install -y  openssl-libs
+sudo dnf install -y  openssl-devel
+sudo dnf install -y  doxygen
+sudo dnf install -y  graphviz
+sudo dnf install -y  mysql-devel
+sudo dnf install -y  vim
+sudo dnf install -y  jq
+sudo dnf install -y  make
+sudo dnf install -y  bzip2-devel
+sudo dnf install -y  libubsan
+sudo dnf install -y  libasan
+sudo dnf install -y  vim
+sudo dnf install -y  httpd
+sudo dnf install -y  mod_fcgid
+sudo dnf install -y  mod_proxy_html
+sudo dnf install -y  mod_ssl
+sudo dnf install -y  patch
+sudo dnf install -y  tcsh
+sudo dnf install -y  curl
+sudo dnf install -y  wget
+sudo dnf install -y  mariadb
+sudo dnf install -y  mariadb-server 
+sudo dnf install -y  emacs
+sudo dnf install -y  node
+sudo dnf install -y  npm
+sudo dnf install -y  golang
+
